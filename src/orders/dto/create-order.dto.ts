52becf1e63092e9445c05404aaa0b0afa78324ab
quochiep16 <quochiep1610@gmail.com>
2025-11-18 22:43:41 +0700

    cấu lại sdt

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsPhoneNumber } from 'class-validator';
+import { IsNotEmpty, IsPhoneNumber, Length, Matches } from 'class-validator';
 
 export class CreateOrderDto {
   @IsNotEmpty({ message: 'Tên người nhận không được để trống' })
@@ -6,6 +6,10 @@ export class CreateOrderDto {
 
   // đơn giản: kiểm tra là string không rỗng, không bắt buộc dùng IsPhoneNumber(VN)
   @IsNotEmpty({ message: 'Số điện thoại không được để trống' })
+  @Length(9, 15, { message: 'Số điện thoại phải có từ 9 đến 15 ký tự.' })
+  @Matches(/^\+?\d+$/, {
+    message: 'Số điện thoại chỉ được chứa chữ số và có thể có dấu "+" ở đầu.',
+  })
   shippingPhone: string;
 
   @IsNotEmpty({ message: 'Địa chỉ giao hàng không được để trống' })
